7b7de2c0057e0d1ec1d35e2ca4d24aa8ac6b9b69
Mike Sarahan <msarahan@gmail.com>
2019-01-25 09:10:50 -0600

    improve repo/pkg download script to be more cross-platform and more complete

--- download_files.py
+++ download_files.py
@@ -2,28 +2,46 @@ import os
 import sys
 import shutil
 from six.moves import urllib
-
-pkgs = []
-with open('flist') as f:
-    for line in f.readlines():
-        name, version = line.split(":")
-        fn = name.strip() + '-' + version.strip() + '.tar.bz2'
-        pkgs.append(fn)
+from conda.exports import subdir
+from conda.common.io import env_vars
+from conda.cli import python_api
 
 pkgs_dir = os.path.join(sys.prefix, 'pkgs')
 
-
-def download_package(channel, subdir, fn, pkg_cache_dir, force=False):
-    cache_folder = os.path.join(pkg_cache_dir, subdir)
-    if not os.path.isdir(cache_folder):
-        os.makedirs(cache_folder)
-    if not os.path.isfile(os.path.join(cache_folder, fn)) and not force:
-        print('Downloading %s' % fn)
-        urllib.request.urlretrieve(
-            "%s/%s/%s" % (channel, subdir, fn),
-            os.path.join(cache_folder, fn))
+channel_map = {
+    'free': 'https://repo.anaconda.com/pkgs/free',
+    'main': 'https://repo.anaconda.com/pkgs/main',
+    'r': 'https://repo.anaconda.com/pkgs/r',
+    'conda-forge': 'https://conda.anaconda.org/conda-forge',
+    'bioconda': 'https://conda.anaconda.org/bioconda',
+}
 
 
-for fn in pkgs:
-    download_package('https://repo.anaconda.com/pkgs/main', 'osx-64', fn,
-                     os.path.join('repos', 'main'))
+with env_vars({
+        "CONDA_PKGS_DIRS": os.path.join('repos', 'main', subdir),
+}):
+    try:
+        python_api.run_command("create", "-n", "fakeenv", "--download-only", "python", "mkl")
+    except:
+        pass
+    if sys.platform != "win32":
+        try:
+            python_api.run_command("create", "-n", "fakeenv", "--download-only", "libopenblas")
+        except:
+            pass
+
+for chan, url in channel_map.items():
+    for _subdir in (subdir, 'noarch'):
+        subdir_path = os.path.join('repos', chan, _subdir)
+        if not os.path.exists(subdir_path):
+            os.makedirs(subdir_path)
+        for fn in ("repodata.json", "repodata.json.bz2"):
+            urllib.request.urlretrieve(
+                "%s/%s/%s" % (url, _subdir, fn),
+                os.path.join(subdir_path, fn))
+
+        # remove subdirs (extracted packages - we want to include package extraction in timing)
+        for _, dirs, _ in os.walk(subdir_path):
+            for dirname in dirs:
+                shutil.rmtree(os.path.join(subdir_path, dirname))
+
